23792bc739df8efa9ce1e1427960f0d8e7608ad6
juhyun <zzang9ha@naver.com>
2023-06-28 19:33:19 +0900

    test: Repository 테스트 코드 추가

---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (id, email, nickname, address, certification_code, status, last_login_at)
+VALUES (1, 'a@a.com', 'Hyun', 'Seoul', 'aaaaaa-aaaa-aaaa-aaaaa-aaaaaaaaaaaa', 'ACTIVE', 0);

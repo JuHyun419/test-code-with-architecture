0b4b32599f49bf0fa09fc68cd6addfb2379e634a
juhyun <zzang9ha@naver.com>
2023-06-28 19:33:28 +0900

    test: Service 테스트 코드 추가

---
+++ src/test/resources/sql/user-service-test-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, email, nickname, address, certification_code, status, last_login_at)
+VALUES (2, 'a@a.com', 'Hyun1', 'Seoul', 'aaaaaa-aaaa-aaaa-aaaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+
+INSERT INTO users (id, email, nickname, address, certification_code, status, last_login_at)
+VALUES (3, 'b@a.com', 'Hyun2', 'Seoul', 'aaaaaa-aaaa-aaaa-aaaaa-aaaaaaaaaaab', 'PENDING', 0);
